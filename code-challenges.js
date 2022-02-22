5a334dd822f617fc36466f8e2096344dd18f5753
Nathaniel Bethea <jumpnate23@outlook.com>
2022-02-22 09:42:22 -0800

    completed assessment

--- code-challenges.js
+++ code-challenges.js
@@ -14,36 +14,104 @@ const fruit2 = "banana"
 const fruit3 = "cherry"
 const fruit4 = "kiwi"
 
+const longerString = (stringOne, stringTwo) => {
+  if(stringOne.length > stringTwo.length) {
+    return stringOne
+  } else {
+    return stringTwo
+  }
+}
+
+console.log(longerString(fruit1, fruit2))
+console.log(longerString(fruit3, fruit4))
 
 // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-const temp1 = 42
-const temp2 = 350
-const temp3 = 212
+// const temp1 = 42
+// const temp2 = 350
+// const temp3 = 212
+ 
+// Psuedo Code
+// a function that takes x and y and gives value of below, at, or above boiling point
+// will need an if/let statement
+// looking for 3 different outputs from given data
+
+
+// if(temp1 < 212) {
+//   console.log(`${temp1} is below boiling point`)
+// } else if(temp2 > 349) {
+//   console.log(`${temp2} is above boiling point`)
+// } else if(temp3 > 42 && temp3 < 349){
+//   console.log(`${temp3} is at boiling point`)
+// } else {
+//   console.log("please enter a number from 0 to 100")
+// }
 
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
-const myNumbers1 = [3, 7, 0, 36, -9]
-const myNumbers2 = [8, -7, 42, 9, 13]
+// const myNumbers1 = [3, 7, 0, 36, -9]
+// const myNumbers2 = [8, -7, 42, 9, 13]
 
+// Pseudo Code
+// need to use an accesor to combine the two constants
+// need to use .length in order to then get the length of the array
 
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
+// if(myNumbers1 + myNumbers2 == true) {
+//   console.log(myNumbers1.length(myNumbers2))
+// } else {
+//   console.log(myNumbers1.concat(myNumbers2))
+// }
 
-const currentCohort = "Alpha 2022"
+// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ahplA"
+
+// const currentCohort = "Alpha 2022"
+
+// Pseudo Code
+// declare a variable
+// reverse the code using an accessor
+// console log the variable
+
+// var longerAlpha =["A", "l", "p", "h", "a", "2", "0", "2", "2"]
+// console.log(longerAlpha.reverse())
 
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
-const myArray = [13, 34, 5, 10, 27, 42]
+// const myArray = [13, 34, 5, 10, 27, 42]
 
+// Psuedo Code
+// create a for statement to return 6 responses
+// use modulo to differ bettween odd vs even
+// use if/else statement to be able to conole log both results
+
+// for (let i = 1; i < 7; i++) {
+//     if (i % 2 === 1 ) {
+//       console.log ("odd")
+//     } else {
+//       console.log("even")
+//     }
+//   }
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
 
 // Set one:
-const number1 = 58
-const number2 = 100
+// const number1 = 58
+// const number2 = 100
 
 // Set two:
-const number3 = 27
-const number4 = 24
+// const number3 = 27
+// const number4 = 24
+
+// Psuedo Code
+// set a parameter
+// will need an if/if else statment
+// will need to log 100-58
+// needs to log 27-23
+
+// const longerNumb = (number1, number2, number3, number4) => {
+//   return number1 - number2
+// } 
+
+// console.log(longerNumb(number2, number1))
+// console.log(longerNumb(number3, number4))
--- interview-questions.md
+++ interview-questions.md
@@ -5,51 +5,51 @@ First, without external resources. Challenge yourself to answer from memory as i
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-INSTRUCTOR EXAMPLE: What is a conditional statement?
+<!-- INSTRUCTOR EXAMPLE: What is a conditional statement?
 
-  Your answer:
+  Your answer: A specific instruction to give a specific answer
 
-  Researched answer:
+  Researched answer: --> Instructions that give a specific answer based on the values that are inputted in.
 
 
 
 1. What is git? What is the difference between git and Github?
 
-  Your answer:
+  Your answer: Git is a system that tracks changes to code, whereas GitHub is a platform in which code can be uploaded to in order for users to share and work on the same code.
 
-  Researched answer:
+  Researched answer: Git is a version control system that works to keep track of your coding history. GitHub is a cloud based platform that allows collaborators, and co-collaborators to manage repositiories and access and share code.
 
 
 
 2. Which JavaScript operators will return a Boolean value?
 
-  Your answer:
+  Your answer: Use of Logical and Relational operators will return a Boolean value.
 
-  Researched answer:
+  Researched answer: Equality, Relational, and Logical operators when used correctly will return a Boolean value.
 
 
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer:
+  Your answer: An index is the place where a character is at in your code, while a value is something you set within the code.
 
-  Researched answer:
+  Researched answer: An index is where a specific character can be accounted for in a string, whereas a value is something you set a variable to.
 
 
 
 4. What is iteration?
 
-  Your answer:
+  Your answer: Iteration is the steps of repeating a set of particular code.
 
-  Researched answer:
+  Researched answer: Iteration is the action of completing a set of code until the desired condition is met.
 
 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: Proper pair programming techniques include: great communication, proper planning of the project or desired outcome, pseudo coding prior to beginning and defined roles of each contributing member such as the driver or navigator. The navigator is in charge of directing the driver in writing the code and direction of project or task, whereas the driver is in charge of receiving the information and coding the necessary elements.
 
-  Researched answer:
+  Researched answer: Pair programming is a technique in which 2 collaborators are able to work on the same project at the same time. Proper pair programming consisits of a driver and a navigator. The driver is in charge of writing the code while the navigator is charge of direction and making sure the code is correct.
 
 
 
@@ -57,12 +57,12 @@ INSTRUCTOR EXAMPLE: What is a conditional statement?
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions:
+1. Higher Order Functions: Higher-Order functions are built in to JavaScript and can used with arrays. They can also take in information and return an output.
 
-2. Jest:
+2. Jest: Jest is framework used to run tests on your code to ensure its effectiveness.
 
-3. Objects:
+3. Objects: Objects are a data type that holds values.
 
-4. Method:
+4. Method: a method is an object with behavior and happens when a function is assigned to an object
 
-5. Classes:
+5. Classes: Classes are used to specify data and attributes.
